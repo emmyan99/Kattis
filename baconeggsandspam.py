ad97f3ed896c6b344b130d72b27bb206b4ca22cf
emmyan99 <emmy.andreasson@gmail.com>
2023-03-23 16:27:22 +0100

    baconeggsandspam

--- baconeggsandspam.py
+++ baconeggsandspam.py
@@ -1,50 +1,40 @@
-
-x = 1
-report = ""
+x=1
+resultList = []               #create list of lists of result dicts
 
 while(x != 0):
-    x = int(input()) #no. of customers
     dict = {}
-
+    x = int(input()) #no. of customers
+    
     for i in range(x): 
         line = input()
         line = line.split() # "Alice bacon eggs spam" -> "['Alice', 'bacon', 'eggs', 'spam']
-        list = []
-        for j in range(1,len(line)):     
-            list.append(line[j])
-            dict[line[0]] = list # {'Alice': ['bacon', 'eggs', 'spam']}
+        
+        for fooditem in range(1,len(line)): # NOTE: fooditem is an int
+            if not(line[fooditem] in dict):       #if fooditem not in dict
+                newList = []                         #create new list
+                newList.append(line[0])              #append name to empty list 
+                dict[line[fooditem]] = newList       #add list of names (value) to fooditem(key)
+                #print("new:" + str(dict))
+            else:
+                existingList = dict[line[fooditem]]
+                existingList.append(line[0])
+                existingList.sort() #sorts list of names lexicographically
+                dict.update({line[fooditem]: existingList}) 
+                #print("existing:" + str(dict))
     
-        for key, lists in dict.items():
-            for fooditem in lists:
-                print("report: " + report)
+    
+    result = sorted(dict.items()) #sort dict, returns tuple
 
-                if not report: # first iteration
-                    report += fooditem + " " + str(key)
-                else:
-                    if not(str(fooditem) in report): # item is not in report
-                        report += "\n" + fooditem + " " + str(key)
-                    else:
-                        #index = report.index(fooditem)
-                        #print(index)
-
-                
-                
-               
-
-
-
-                    
-
-                
-
-
-
+    listConversion = []
+    listConversion = list(result)         #convert tuple to list 
+    resultList.append(listConversion)     #add list to resultList
     
 
-
-
-
-
-
-
-
+for list in resultList: #list: [('ham', ['Joe', 'Sue']), ('spam', ['Joe'])]
+    if (list):
+        for element in list: # element: ('ham', ['Joe', 'Sue'])
+            print(str(element[0]), end=" ") # element[0] == ham
+            for names in element[1]:
+                print(str(names), end=" ")
+            print("\n", end="")
+        print("")
---
+++ baconeggsandspamWrong.py
@@ -0,0 +1,50 @@
+
+x = 1
+report = ""
+
+while(x != 0):
+    x = int(input()) # no. of customers
+    dict = {}
+
+    for i in range(x): 
+        line = input()
+        line = line.split() # "Alice bacon eggs spam" -> "['Alice', 'bacon', 'eggs', 'spam']
+        list = []
+        for j in range(1,len(line)):     
+            list.append(line[j])
+            dict[line[0]] = list # {'Alice': ['bacon', 'eggs', 'spam']}
+    
+        for key, lists in dict.items():
+            for fooditem in lists:
+                print("report: " + report)
+
+                if not report: # first iteration
+                    report += fooditem + " " + str(key)
+                else:
+                    if not(str(fooditem) in report): # item is not in report
+                        report += "\n" + fooditem + " " + str(key)
+                    else:
+                        #index = report.index(fooditem)
+                        #print(index)
+
+                
+                
+               
+
+
+
+                    
+
+                
+
+
+
+    
+
+
+
+
+
+
+
+
