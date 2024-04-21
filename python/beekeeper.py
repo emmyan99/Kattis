6b665f2ad14590ec9854f26e89690128130af07a
emmyan99 <emmy.andreasson@gmail.com>
2024-04-21 13:46:09 +0200

    beekeeper done

--- python/beekeeper.py
+++ python/beekeeper.py
@@ -1,61 +1,51 @@
 def main():
     count = 0
     vowels = "aeiouy"
+    results = []
 
     while(count < 10):
         testCaseNo = int(input())
         count += 1
-        output = []
         
         if testCaseNo == 0: break
         else: 
+            dict = {}
+            doubleCounts = []
             while(testCaseNo > 0):
-                dict = {}
                 word = input()
                 testCaseNo -= 1
 
                 index = 0
                 positions = []
                 for char in word:
-                    if vowels.find(char) > 0:
+                    if vowels.find(char) != -1:
                         positions.append(index)
-                        #print("vowelfiound")
                     else:
                         pass
-                        print(char)
                     index += 1
-                    #print(index)
-                index = 0
 
-                #print(positions)
                 doubleCount = 0
                 for i in positions:
                     for j in positions:
-                        if j - i == 1:
-                            #print(i, j)
+                        if i - j == 1 and word[i] == word[j]:
                             doubleCount += 1
-                #print(doubleCount)
-                
-                dict[word] = doubleCount
-                #print(dict)
+                doubleCounts.append(doubleCount)
 
+                #word = key, doubleCount = value
+                dict[doubleCount] = word
+        #print(doubleCounts) 
+        highestDoubleCount = max(doubleCounts)
+        #print("highest: ", highestDoubleCount)
+        results.append(dict[highestDoubleCount])
+        #print(results)
 
-                
-
-
-    
+    for x in results:
+        print(x)
                     
-
-
-
 #"beekeeper"
 # index of vowels
 # if two indexes are next to each other
 # add count for each of these cases
 # max wins
-            
-
-
-
 
 main()
