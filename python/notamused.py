0c89443f677aff2082f9d26e2ca2f4815573d20e
emmyan99 <emmy.andreasson@gmail.com>
2024-04-24 20:41:12 +0200

    notamused

--- python/notamused.py
+++ python/notamused.py
@@ -9,19 +9,22 @@ def main():
 
 def open():
     msg = input()
-    entered = set()
-    exited = {}
+    #entered = set()
+    exited = {{}}
+    #exited = {
+    #       "name" :{
+    #   "entrymin" : 5,
+    #   "exitmin"  : 35
+    #},
     name_pattern = r'^(EXIT|ENTER)\s+(\w+)\s+\d{1,3}$'
 
     if msg == "CLOSE":
        return
     else:
         if msg.find("ENTER") != -1:
-            entered.add(re.match(name_pattern, msg).group(2))
-            for name in entered:
-                print(name)
-        
+            #entered.add(re.match(name_pattern, msg).group(2))
 
+        elif msg.find("EXIT") != -1:
 
 
 
