ae341f4ef8d6395da9e53d2f0da2cef194e703dc
emmyan99 <emmy.andreasson@gmail.com>
2024-05-10 16:23:41 +0200

    very ugly but it works(??)

--- python/notamused.py
+++ python/notamused.py
@@ -30,27 +30,27 @@ def main():
                 name = re.match(name_pattern, entry).group(2)
                 entrymin = int((entry.partition(name))[2])
                 if find_in_dict(name, details):
-                    for dict in details:
-                        if dict["name"] == name:
-                            dict["entrymin"] == entrymin
+                    for detail in details:
+                        if detail["name"] == name:
+                            detail["entrymin"] = entrymin
                 else:
                     newentry = {"name": name, "entrymin": entrymin, "price": 0} #price will be calc later, entrymin until EXIT
-                    print(newentry)
                     details.append(newentry)
 
             elif entry.find("EXIT") != -1:
                 name = re.match(name_pattern, entry).group(2)
                 exitmin = int((entry.partition(name))[2])
-                for dict in details:
-                    if dict["name"] == name:
-                        # print(dict["name"])
-                        dict["price"] == dict["price"] + ((exitmin - dict["entrymin"])*0.1)
-                        # print(dict["price"] + (exitmin-dict["entrymin"])*0.1)
-                        #last price is wrong
-                        dict["entrymin"] == 0
+                for detail in details:
+                    if detail["name"] == name:
+                        detail["price"] = detail["price"] + ((exitmin - detail["entrymin"])*0.1)
+
+                        detail["entrymin"] = 0
+
+        details = sorted(details, key=lambda x: list(x.values())[0])
+        print(details)
         days.append(details)
-
-
+        
+    printLog(days)
 
 def find_in_dict(val, list_of_dicts):
     for dict in list_of_dicts:
@@ -59,54 +59,15 @@ def find_in_dict(val, list_of_dicts):
     return False
 
 
+def printLog(dayslog):
+    dayCounter = 1
+    for day in dayslog:
+        print("Day ", dayCounter)
+        dayCounter += 1
 
+        for entry in day:
+            # print(entry["name"], " $",(f'{entry["price"]:.2f}'))
+            print('{} ${}'.format(entry["name"],(f'{entry["price"]:.2f}')))
 
-
-
-#(exitmin - entrymin) * 0.1
-
-
-
-
-
-
-
-
-
-        #while line != "CLOSE":
-
-             #   print(line)
-             #   daylines.append(line)
-
-            
-
-#def open():
-    #while(True):
-        #msg = input()
-    #entered = set()
-    #exited = {}
-    #exited = {
-    #       "name" :{
-    #   "entrymin" : 5,
-    #   "exitmin"  : 35
-    #},
-    #name_pattern = r'^(EXIT|ENTER)\s+(\w+)\s+\d{1,3}$'
-
-    #if msg == "CLOSE":
-       #return
-    #else:
-        #if msg.find("ENTER") != -1:
-            #name = re.match(name_pattern, msg).group(2)
-            #entrymin = int((msg.partition(name))[2])
-            #newEntry = {'entrymin':entrymin, 'exitmin':0}
-            #exited[name] = newEntry 
-            #for x in exited.values():
-            #    print(x)
-        #elif msg.find("EXIT") != -1:
-            #name = re.match(name_pattern, msg).group(2)
-            #exitmin = int((msg.partition(name))[2])
-            #exited[name][exitmin] = exitmin
-            #for x in exited.values():
-                #print(x)
 
 main()
